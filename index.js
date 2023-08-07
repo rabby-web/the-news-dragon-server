9e6a468c2f4cf05d16fbea7c9fd510c116e434d0
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-08 01:58:44 +0600

    news data load

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
 
+const news = require('./data/news.json');
+
 app.use(cors());
 
 
@@ -15,6 +17,23 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) =>{
     console.log(categories)
     res.send(categories);
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
+    const categoryNews = news.filter(n => n.category_id === id);
+    res.send(categoryNews);
 })
 
 app.listen(port, () => {
